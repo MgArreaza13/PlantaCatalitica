a225d32425c7b084121b220d0c9eb7c8227182ca
MgArreaza13 <mg.arreaza.13@gmail.com>
2017-02-10 09:55:05 -0400

    se trabajo en el front de la parte de gastos de viaje

--- GViaje.php
+++ GViaje.php
@@ -196,7 +196,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Informacion de tu grupo
+                            Gastos de Viaje
                         </h1>
                         <ol class="breadcrumb">
                             <li>
